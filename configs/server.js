0320d27834c2d9abd4ba0ec3b667719de696cec6
closertb <closertb@sina.com>
2019-06-19 08:04:41 +0800

    feat: alter server name

--- configs/server.js
+++ configs/server.js
@@ -3,10 +3,9 @@ const servers = {
     admin: 'http://127.0.0.1:8080'
   },
   production: {
-    admin: 'http://127.0.0.1:8080/client'
+    admin: '//server.closertb.site/client'
   }
 };
 
 const server = servers[process.env.NODE_ENV];
-console.log('server', process.env.NODE_ENV, server);
 export default server;
